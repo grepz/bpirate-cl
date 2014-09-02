05903423e7cf848d4038d958f867168e3f006d8d
Stanislav M. Ivankin <lessgrep@gmail.com>
2014-09-02 14:12:02 +0900

    * src/serial.lisp: vtime/vmin tcattr parameters setup added.

--- src/serial.lisp
+++ src/serial.lisp
@@ -4,17 +4,27 @@
 
 (defconstant +FIONREAD+ #x541B)
 
-(defun serial-open (path baud)
-  (let* ((dev (sb-posix:open
-	       path (logior sb-posix:O-NOCTTY sb-posix:O-NDELAY
-			    sb-posix:O-RDWR sb-posix:O-NONBLOCK)))
-	 (tcattr (sb-posix:tcgetattr dev)))
-    (setf (sb-posix:termios-cflag tcattr)
-	  ;; baud | CS8 | CREAD | CLOCAL
-	  (logior sb-posix:CS8 sb-posix:CREAD sb-posix:CLOCAL baud))
-    (sb-posix:tcflush dev sb-posix:TCIFLUSH)
-    (sb-posix:tcsetattr dev sb-posix:TCSAFLUSH tcattr)
-    (make-fd-stream dev :input t :output t :buffering :none
+(defun serial-open (path baud &key (vmin 0) (vtime 1))
+  ;; sb-posix:O-NDELAY sb-posix:O-NONBLOCK
+  (let* ((fd (sb-posix:open path (logior sb-posix:O-NOCTTY sb-posix:O-RDWR)))
+	 (old (sb-posix:tcgetattr fd))
+	 (new (sb-posix:tcgetattr fd)))
+    (setf
+     ;; LFLAG
+     (sb-posix:termios-lflag new)
+     (logandc2 (sb-posix:termios-lflag old)
+     	      (logior sb-posix:icanon sb-posix:echo sb-posix:echoe
+     		      sb-posix:echok sb-posix:echonl))
+     ;; CFLAG: baud | CS8 | CREAD | CLOCAL
+     (sb-posix:termios-cflag new)
+     (logior sb-posix:CS8 sb-posix:CREAD sb-posix:CLOCAL baud)
+     ;; VMIN
+     (aref (sb-posix:termios-cc new) sb-posix:vmin) vmin
+     ;; VTIME
+     (aref (sb-posix:termios-cc new) sb-posix:vtime) vtime)
+    (sb-posix:tcflush fd sb-posix:TCIFLUSH)
+    (sb-posix:tcsetattr fd sb-posix:TCSAFLUSH new)
+    (make-fd-stream fd :input t :output t :buffering :none
 		    :element-type '(unsigned-byte 8))))
 
 (defun serial-close (s)
@@ -27,13 +37,13 @@
     (sb-posix:ioctl (fd-stream-fd s) +FIONREAD+ (sb-alien:addr bytes))
     bytes))
 
-(defun serial-read (s)
+(defun serial-read (s &key)
   (let ((n (serial-recv-len s)))
-    (unless (zerop n)
+    (unless (zerop n))
       (let ((res (make-sequence 'vector n)))
 	(loop for i from 0 below n do
 	     (setf (aref res i) (read-byte s))
-	   finally (return res))))))
+	   finally (return res)))))
 
 (defun serial-write (s data)
   (declare (vector data))
